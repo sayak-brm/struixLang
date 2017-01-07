0585d4795ed5bcd70fa94427a9562e08c1c0f8ff
Sayak Brahmachari <admin@microcontrolled.nl>
2017-01-07 09:07:40 +0530

    Fixing Conflicts before pulling

---
+++ repl.py
@@ -0,0 +1,35 @@
+##   Copyright 2016 Sayak Brahmachari
+##
+##   Licensed under the Apache License, Version 2.0 (the "License");
+##   you may not use this file except in compliance with the License.
+##   You may obtain a copy of the License at
+##
+##       http://www.apache.org/licenses/LICENSE-2.0
+##
+##   Unless required by applicable law or agreed to in writing, software
+##   distributed under the License is distributed on an "AS IS" BASIS,
+##   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##   See the License for the specific language governing permissions and
+##   limitations under the License.
+
+import code
+import sys
+
+import struixTerp
+import struixPrimitives
+
+sys.ps1 = "sxL> "
+sys.ps2 = ">>>> "
+banner = "struixLang REPL v1.0.20161017a.\nPress Ctrl-D to quit."
+
+class Shell(code.InteractiveConsole):
+    def runsource(self, source, filename="<stdin>"):
+        try:
+            terp.run(source)
+        except:
+            self.showsyntaxerror(filename)
+        return False
+
+terp = struixTerp.Terp()
+struixPrimitives.AddWords(terp, terp.ENABLE_UNSAFE_OPERATIONS)
+Shell().interact(banner)
