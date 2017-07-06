62cfa49ca177e2a5367297b55c1dcc63ec70777b
Sayak Brahmachari <admin@microcontrolled.nl>
2017-07-06 23:03:04 +0530

    Remove baggage

--- partpy/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Taylor "Nekroze" Lawson'
-__email__ = 'nekroze@eturnilnetwork.com'
-
-from .sourcestring import SourceString
-from .sourcestring import SourceLine
-from .partpyerror import PartpyError
